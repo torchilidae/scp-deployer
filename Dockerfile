bd39b58ccd03ac16557232ba6c2dd0221f517fcd
siva1024 <siva.radium@gmail.com>
2021-04-17 19:19:01 -0700

    Test to print ID

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,4 @@ FROM alpine
 RUN apk update && apk add git openssh
 COPY entrypoint.sh /entrypoint.sh
 RUN chmod +x /entrypoint.sh
-RUN addgroup -S torch && adduser -S torch -G torch -s /bin/sh -h /home/torch
-USER torch
-WORKDIR /home/torch
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,69 +1,26 @@
 #!/bin/sh
 echo "+++++++++++++++++++STARTING PIPELINES+++++++++++++++++++"
 
-id
-echo ""
-pwd
-echo ""
-cd ~ && pwd
-echo ""
-
 exitApplication () {
     echo "Exiting the Application"
     exit 1
 }
 
 sshSetup(){
-    echo "Setting up the SSH folders"
-    mkdir ~/.ssh/ && chmod 0700 ~/.ssh/
-    export HOST_IP=`getent hosts "$3" | awk '{ print $1 }'`
-    echo $HOST_IP
-    ssh-keyscan github.com > ~/.ssh/known_hosts
-    ssh-keyscan $3 >> ~/.ssh/known_hosts
-    if [ "$HOST_IP" ]; then
-        ssh-keyscan "$HOST_IP" >> ~/.ssh/known_hosts
-    fi
-
     echo "Setting up the public, private keys and Executables"
-    echo "-----BEGIN OPENSSH PRIVATE KEY-----" >> ~/.ssh/id_rsa
-    echo $1 | tr " " "\n" | sed '1,4d' | tac | sed '1,4d' | tac >> ~/.ssh/id_rsa
-    echo "-----END OPENSSH PRIVATE KEY-----" >> ~/.ssh/id_rsa
-    echo "" >> ~/.ssh/id_rsa
-    echo $2 > ~/.ssh/id_rsa.pub
-    chmod 600 ~/.ssh/id_rsa && chmod 600 ~/.ssh/id_rsa.pub
-    ls -ltr ~/.ssh/
+    echo "-----BEGIN OPENSSH PRIVATE KEY-----" >> tmp_id
+    echo $1 | tr " " "\n" | sed '1,4d' | tac | sed '1,4d' | tac >> tmp_id
+    echo "-----END OPENSSH PRIVATE KEY-----" >> tmp_id
+    echo "" >> tmp_id
 }
 
 scpTransfer(){
-    scp -qr -P $1 -o ConnectTimeout=$2 "$5" "$3"@"$4":"$6"
-    echo "scp -qr -P $1 -o ConnectTimeout=$2 "$5" "$3"@"$4":"$6""
+    scp -qr -P $1 -i tmps_id -o ConnectTimeout=$2 "$5" "$3"@"$4":"$6"
 }
 
 sshpassTransfer(){
     sshpass -p $7 scp -qr -P $1 -o ConnectTimeout=$2 "$5" "$3"@"$4":"$6"
-    echo "sshpass -p $7 scp -r -P $1 -o ConnectTimeout=$2 "$5" "$3"@"$4":"$6""
 }
-
-echo "Checking the configurations"
-if [[ -z "$INPUT_HOST" || -z "$INPUT_USERNAME" ]]; then
-    echo "No Host and user data found to connect"
-    exitApplication
-else
-    echo "Host and User Data Found"
-    echo "Host:"$INPUT_HOST "User:" $INPUT_USERNAME
-fi
-
-if [[ -z "$INPUT_KEY" && -z "$INPUT_PASSWORD" ]]; then
-    echo "No Password or SSH keys detected"
-    exitApplication
-elif [[ "$INPUT_KEY" && -z "$INPUT_PUB" ]]; then
-    echo "No Public key detected for the private key"
-    exitApplication
-else
-    echo "Password/SSH Keys detected"
-    sshSetup "$INPUT_KEY" "$INPUT_PUB" "$INPUT_HOST"
-fi
-echo ""
 
 if [[ "$INPUT_KEY" ]]; then
     scpTransfer $INPUT_PORT $INPUT_CONNECT_TIMEOUT $INPUT_USERNAME $INPUT_HOST $INPUT_SOURCE $INPUT_TARGET
