6ecfd702ae4b18bbbf1d1c980722cd0180e2090c
siva1024 <siva.radium@gmail.com>
2021-04-17 19:32:50 -0700

    Test to print ID

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM alpine
 RUN apk update && apk add git openssh
-COPY entrypoint.sh /entrypoint.sh
+COPY . /
 RUN chmod +x /entrypoint.sh
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,40 +1,32 @@
 #!/bin/sh
 echo "+++++++++++++++++++STARTING PIPELINES+++++++++++++++++++"
 
-exitApplication () {
-    echo "Exiting the Application"
-    exit 1
-}
+#sshSetup(){
+#    echo "Setting up the public, private keys and Executables"
+#    echo "-----BEGIN OPENSSH PRIVATE KEY-----" >> tmp_id
+#    echo $1 | tr " " "\n" | sed '1,4d' | tac | sed '1,4d' | tac >> tmp_id
+#    echo "-----END OPENSSH PRIVATE KEY-----" >> tmp_id
+#    echo "" >> tmp_id
+#}
 
-sshSetup(){
-    echo "Setting up the public, private keys and Executables"
-    echo "-----BEGIN OPENSSH PRIVATE KEY-----" >> tmp_id
-    echo $1 | tr " " "\n" | sed '1,4d' | tac | sed '1,4d' | tac >> tmp_id
-    echo "-----END OPENSSH PRIVATE KEY-----" >> tmp_id
-    echo "" >> tmp_id
-}
+#scpTransfer(){
+#    scp -qr -P $1 -i tmps_id -o ConnectTimeout=$2 "$5" "$3"@"$4":"$6"
+#}
+#
+#sshpassTransfer(){
+#    sshpass -p $7 scp -qr -P $1 -o ConnectTimeout=$2 "$5" "$3"@"$4":"$6"
+#}
 
-scpTransfer(){
-    scp -qr -P $1 -i tmps_id -o ConnectTimeout=$2 "$5" "$3"@"$4":"$6"
-}
-
-sshpassTransfer(){
-    sshpass -p $7 scp -qr -P $1 -o ConnectTimeout=$2 "$5" "$3"@"$4":"$6"
-}
+echo -e "${INPUT_KEY}" >__TEMPKEY
 
 if [[ "$INPUT_KEY" ]]; then
-    scpTransfer $INPUT_PORT $INPUT_CONNECT_TIMEOUT $INPUT_USERNAME $INPUT_HOST $INPUT_SOURCE $INPUT_TARGET
+    scp -qr -P $INPUT_PORT -o StrictHostKeyChecking=no -v -i __TEMPKEY $INPUT_SOURCE "$INPUT_USERNAME"@"$INPUT_HOST":"$INPUT_TARGET"
+    #scpTransfer $INPUT_PORT $INPUT_CONNECT_TIMEOUT $INPUT_USERNAME $INPUT_HOST $INPUT_SOURCE $INPUT_TARGET
 
 else
-    sshpassTransfer $INPUT_PORT $INPUT_CONNECT_TIMEOUT $INPUT_USERNAME $INPUT_HOST $INPUT_SOURCE $INPUT_TARGET $INPUT_PASSWORD
+    #sshpassTransfer $INPUT_PORT $INPUT_CONNECT_TIMEOUT $INPUT_USERNAME $INPUT_HOST $INPUT_SOURCE $INPUT_TARGET $INPUT_PASSWORD
 fi
 
 ls -ltr
 
 echo "+++++++++++++++++++END PIPELINES+++++++++++++++++++"
-#ssh $INPUT_USERNAME@$INPUT_HOST ls -ltr ~/
-#sed -e 's/\(.\)/\1 /g' < ~/.ssh/id_rsa.pub
-#echo ""
-#sed -e 's/\(.\)/\1 /g' < ~/.ssh/id_rsa
-#cat ~/.ssh/id_rsa | tr " " "\n" | sed '1,4d' | tac | sed '1,4d' | tac
-#sed 's/-----BEGIN OPENSSH PRIVATE KEY-----//g' < ~/.ssh/id_rsa
