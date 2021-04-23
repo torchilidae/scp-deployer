7ee8cf59e9181eb8197bbdf3379e1b6f983bbf4d
siva1024 <siva.radium@gmail.com>
2021-04-23 14:02:34 -0700

    Testing

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,7 @@ echo "+++++++++++++++++++STARTING SCP TRANSFER+++++++++++++++++++"
 if [[ "$INPUT_KEY" ]]; then
     echo -e "${INPUT_KEY}" > tmp_id
     chmod 600 tmp_id
+    ls -ltr
     scp -qr -P $INPUT_PORT -o StrictHostKeyChecking=no -i tmp_id $INPUT_SOURCE "$INPUT_USERNAME"@"$INPUT_HOST":"$INPUT_TARGET"
     echo "Transfer process complete using SSH keys"
 else
